ecd4e5ff49c347e4135e840fb0ac9379f89a2186
Cryofbb <78804095+Cryofbb@users.noreply.github.com>
2021-08-07 15:10:53 +0300

    VerifycationPage added

--- src/test/java/ru/netology/page/VerificationPage.java
+++ src/test/java/ru/netology/page/VerificationPage.java
@@ -1,4 +1,20 @@
 package ru.netology.page;
 
-public class VerificationPage {
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.support.FindBy;
+import ru.netology.data.DataHelper;
+
+import static com.codeborne.selenide.Selenide.page;
+
+public class VerifycationPage {
+    @FindBy(css = "[data-test-id=code] input")
+    private SelenideElement verifyField;
+    @FindBy(css = "[data-test-id=action-verify]")
+    private SelenideElement verifyButton;
+
+    public TransferPage validationTrue(DataHelper.VerificationCode code) {
+        verifyField.setValue(code.getCode());
+        verifyButton.click();
+        return page(TransferPage.class);
+    }
 }
